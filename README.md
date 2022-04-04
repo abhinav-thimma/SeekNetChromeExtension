8662d391ceed6d7906dab8ec3c66e36eea22913e
Kevin <kevinros1190@gmail.com>
2022-04-04 15:41:51 -0500

    first update

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# chromeExtensionFrontend
+# SeekNet - Chrome Extension Frontend
+## What is this?
+SeekNet is a way to create and discover connections among webpages. Opening the extension on a webpage displays a list of created outgoing connections with their respective descriptions. Clicking a connection will open a new tab at the connected URL. 
+
+Currently, all connections are user-created. To add a connection to a webpage, open the extension on the webpage and add the connection URL and description. Then click "Create Connection". Your connection should then appear in the list of connections for that current webpage.
+
+In essence, SeekNet helps you discover where to go next.
+## How to add
+1. Clone the repository
+2. In the chrome browser, navigate to chrome://extensions/
+3. Toggle on "Developer Mode" (at the top right of the screen)
+4. Click "Load Unpacked", and select the cloned repository
+5. Click the "Extensions" icon in the Chrome toolbar (puzzle at top right of toolbar)
+6. Pin "SeekNet"
+7. Start browsing and making connections!
---
+++ connectionHandler.js
@@ -0,0 +1,99 @@
+// Change for local development
+const CONNECTION = "http://127.0.0.1:5000/";
+
+var HttpClient = function() {
+    this.get = function(aUrl, aCallback) {
+        var anHttpRequest = new XMLHttpRequest();
+        anHttpRequest.onreadystatechange = function() { 
+            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+                aCallback(anHttpRequest.responseText);
+        }
+        anHttpRequest.open( "GET", aUrl, true );            
+        anHttpRequest.send( null );
+    }
+    this.post = function(aUrl, data, aCallback) {
+        var anHttpRequest = new XMLHttpRequest();
+            anHttpRequest.onreadystatechange = function() { 
+                if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+                    aCallback(anHttpRequest.responseText);
+            }
+            anHttpRequest.open( "POST", aUrl, true );            
+            anHttpRequest.send( data );
+    }
+}
+var client = new HttpClient();
+var connectionForm = document.getElementById('post-connection');
+var connections = document.getElementById('connections');
+
+function getConnections() {
+    url = window.url
+    get_url = CONNECTION + '?url=' + url
+    client.get(get_url, function(response){
+        var parsed_response = JSON.parse(response);
+        if(parsed_response['status'] == 1){
+            renderConnections(parsed_response['payload']['connections'])
+        } else {
+            alert('Sorry, something went wrong. Please try again later.')
+        }
+    })
+}
+
+connectionForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    var data = new FormData(connectionForm);
+    data.append('current_url', window.url)
+    client.post(CONNECTION + 'connect', data, function(response){
+        var parsed_response = JSON.parse(response);
+        if(parsed_response['status'] == 1){
+            renderConnections(parsed_response['payload']['connections'])
+        } else {
+            alert('Sorry, something went wrong. Please try again later.')
+            console.log(parsed_response)
+        }
+    });
+});
+
+
+// render connections
+function renderConnections(parsed_response) {
+    connections.innerHTML = "<text> Connections from the Current URL: </text>"
+    for(var connection in parsed_response){
+        connections.innerHTML += '<hr><text>' + parsed_response[connection]['text'] + '<text><br>'
+        connections.innerHTML += '<button \
+                                    id=' + connection + ' \
+                                    data-url=' + parsed_response[connection]['tgt_url'] + ' \
+                                    data-id=' + parsed_response[connection]['_id'] + '\
+                                    >See Connection </button>'
+        host = new URL(parsed_response[connection]['tgt_url']).host
+        connections.innerHTML += '<text> via </text><b> ' + host + '</b><br>'
+    } 
+    for (var connection in parsed_response) {
+        document.getElementById(connection).addEventListener("click", clickConnection);
+    }
+}
+
+
+function clickConnection(e) {
+    url = e.target.dataset.url
+    var redirectWindow = window.open(url, '_blank');
+    redirectWindow.location;
+    var data = new FormData();
+    data.append('id', e.target.dataset.id)
+    client.post(CONNECTION + 'log', data, function(response){
+        return
+    });
+}
+
+
+
+function handleExtensionOpen() {
+  // gets the URL of the window then renders the data
+  chrome.tabs.query({active: true, currentWindow: true}, 
+    (tabs) => {
+        var url = tabs[0].url;
+        window.url = url
+        getConnections()
+  });
+}
+
+window.addEventListener('load', handleExtensionOpen)
