eab68300c412ed6f8ef02774f51b5092d669d4f0
Abhinav Thimma <athimma2@illinois.edu>
2022-04-27 21:39:29 -0500

    Added section for connections related to current url

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '.././App.css';
 import ManualConnection from './ManualConnection';
 import SearchSeekNet from './SearchSeekNet';
@@ -7,7 +7,21 @@ import { Button, Form, Card } from 'react-bootstrap';
 
 function Home(props: any) {
   const [seeknetSearchResults, setSeeknetSearchResults] = useState([]);
+  const [urlSearchResults, setUrlSearchResults] = useState([]);
   const [query, setQuery] = useState('');
+  const currentUrl = props.url;
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' },
+    };
+
+    fetch('http://127.0.0.1:5000/?url=' + currentUrl, requestOptions)
+      .then(response => response.json())
+      .then(data => setUrlSearchResults(data['payload']['connections']));
+      console.log(currentUrl);
+  }, [setUrlSearchResults, currentUrl]);
 
   const handleSubmit = (event: any) => {
     event.preventDefault();
@@ -23,22 +37,26 @@ function Home(props: any) {
       .then(data => setSeeknetSearchResults(data['results']));
   };
 
-  const handleLinkClick = (e: any) => {
-    console.log(e);
+  const handleLinkClick = async(e: any) => {
+    console.log('Clicked: ', e);
 
     const requestOptions = {
       method: 'POST',
       headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' },
-      body: JSON.stringify({ id: e.target.id })
+      body: JSON.stringify({ src_url: currentUrl, tgt_url: e.target.href, search_text: query})
     };
 
-    fetch('http://127.0.0.1:5000/log', requestOptions)
-      .then(response => response.json());
+    await fetch('http://127.0.0.1:5000/log_clicks', requestOptions)
+      .then(response => response.json())
+      .then(data => console.log(data));
+    
+    window.open(e.target.href);
   };
 
   return (
     <div className="App">
       <header className="App-header">
+        <h3 style={{color:"white", fontWeight: "bold"}}>SeekNet</h3>
         <div>
           <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="formBasicQuery">
@@ -51,26 +69,41 @@ function Home(props: any) {
           </Form>
         </div>
         <br />
-        <div id="SearchResults" className="SearchResults"> 
+        <div id="SearchResults" className="SearchResults">
           {seeknetSearchResults.length > 0 &&
             <Card>
-              <Card.Header>Seeknet results</Card.Header>
+              <Card.Header style={{color:"black", fontWeight: "bold"}}>Seeknet results</Card.Header>
               <Card.Body>
                 {seeknetSearchResults.map((result: any, index: number) => (
                   <div>
                     <Card.Link href={result.url}  id={result.id} onClick={handleLinkClick}>{result.text}</Card.Link>
-                    <br/>
+                    <br />
                   </div>
                 ))}
               </Card.Body>
             </Card>
           }
         </div>
-        <br/>
-        <Button variant="info" className="AppButton" onClick={() => goTo(ManualConnection, {url: props.url})}>Manual Connection</Button>
         <br />
-        <Button variant="info" className="AppButton" onClick={() => goTo(SearchSeekNet, {url: props.url})}>Search Seeknet</Button>
+        <Button variant="info" className="AppButton" onClick={() => goTo(ManualConnection, { url: props.url })}>Manual Connection</Button>
+        <br />
+        {/* <Button variant="info" className="AppButton" onClick={() => goTo(SearchSeekNet, {url: props.url})}>Search Seeknet</Button> */}
+        <div id="urlConnections" className="urlConnections">
+          {urlSearchResults.length > 0 &&
+            <Card>
+              <Card.Header style={{color:"black", fontWeight: "bold"}}>Connection with current URL as source</Card.Header>
+              <Card.Body>
+                {urlSearchResults.map((result: any, index: number) => (
+                  <div>
+                    <Card.Link href={result.tgt_url} id={result.id} onClick={handleLinkClick}>{result.text}</Card.Link>
+                    <br />
+                  </div>
+                ))}
+              </Card.Body>
+            </Card>
+          }
 
+        </div>
       </header>
     </div>
   );
